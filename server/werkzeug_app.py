d6af50a933e55eaee7b1863498dbd0869f326611
Brian Kibet <kibetsbrian@gmail.com>
2023-09-18 14:49:45 +0300

    Completed assignment

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,16 @@
-#!/usr/bin/env python3
+#!/home/moringa/.local/share/virtualenvs/python-p4-web-servers-and-wsgi-dzMEXaK1/bin/python
+
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    return Response('A WSGI generated this response!')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+        hostname='localhost',
+        port=5555,
+        application=application
+    )
